5ffb041811f12bede7964553f77e6afde2fefbe3
RichardGrac <richard_grac@hotmail.com>
2020-01-09 16:51:15 -0600

    Auto-login and auto-logout

--- src/App.js
+++ src/App.js
@@ -14,20 +14,76 @@ import Auth from './user/pages/Auth';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import {AuthContext} from './shared/context/auth-context';
 
+let logoutTimer
+
 const App = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [userId, setUserId] = useState(0);
     const [userToken, setUserToken] = useState('')
 
+    React.useEffect(() => {
+        getToken()
+    }, [])
+
+    const getToken = async () => {
+        const token = await localStorage.getItem('token')
+        const userId = await localStorage.getItem('userId')
+        if (token) {
+            const expirationDate = new Date(localStorage.getItem('expirationDate'))
+            const actualDate = new Date()
+            if (expirationDate > actualDate) {
+                setUserToken(token)
+                setUserId(userId)
+                setIsLoggedIn(true)
+
+                autoLogOut(expirationDate, actualDate)
+            } else {
+                removeToken()
+            }
+        }
+    }
+
+    const autoLogOut = (expirationDate, actualDate) => {
+        logoutTimer = setTimeout(() => {
+            removeToken()
+            setUserToken('')
+            setUserId(0)
+            setIsLoggedIn(false)
+        }, (expirationDate.getTime() - actualDate.getTime()))
+    }
+
+    const setToken = (token, usrId, expirationDate) => {
+        localStorage.setItem('token', token)
+        localStorage.setItem('userId', usrId)
+        localStorage.setItem('expirationDate', expirationDate.toISOString())
+    }
+
+    const removeToken = async () => {
+        try {
+            localStorage.removeItem('token')
+            localStorage.removeItem('userId')
+            localStorage.removeItem('expirationDate')
+        } catch (e) {
+            console.error('Could not remove -token or userId- from local storage')
+        }
+
+    }
+
     const login = useCallback((userId, token) => {
+        const expirationDate = new Date(new Date().getTime() + 1000 * 60 * 60)
+        const actualDate = new Date()
+
         setIsLoggedIn(true);
         setUserId(userId)
         setUserToken(token)
+        setToken(token, userId, expirationDate)
+        autoLogOut(expirationDate, actualDate)
     }, []);
 
     const logout = useCallback(() => {
         setIsLoggedIn(false);
         setUserId(0)
+        removeToken()
     }, []);
 
     let routes;
